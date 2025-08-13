c735065e93fb44b6c2095cb77edf31eb538cebdf
0mar067 <elnabaseugine@gmail.com>
2025-08-13 17:36:57 +0300

    instance methods

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,15 @@
 class Person:
     # Class body goes here
 
-    #Instance method definition
-    pass
+     #Instance method definition
+    
+     def talk(self):
+         print("Hello World!")
+
+     def walk(self):
+         print("The person is walking.")
+john = Person()  # Create an instance of the Person class
+john.talk()   # Call the talk method on the second instancedef walk(self):
+        
+jane = Person()
+jane.walk()
